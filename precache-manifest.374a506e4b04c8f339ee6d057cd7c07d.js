9f98a7391865a676035ee4d7a79b4b05c8d16d6c
Bryce <bryce.senninger@gmail.com>
2019-09-28 17:27:50 -0600

    Updates

---
+++ precache-manifest.374a506e4b04c8f339ee6d057cd7c07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4afe14493b04c6ec3ea535a6a65545",
+    "url": "/ReactMemoryGame/index.html"
+  },
+  {
+    "revision": "f50555b7c1fff1424f1a",
+    "url": "/ReactMemoryGame/static/css/main.f0790404.chunk.css"
+  },
+  {
+    "revision": "e08cdf254e6015fe197e",
+    "url": "/ReactMemoryGame/static/js/2.90b068db.chunk.js"
+  },
+  {
+    "revision": "f50555b7c1fff1424f1a",
+    "url": "/ReactMemoryGame/static/js/main.396adecd.chunk.js"
+  },
+  {
+    "revision": "5f60813e77c63619c860",
+    "url": "/ReactMemoryGame/static/js/runtime-main.1049ada0.js"
+  },
+  {
+    "revision": "dd07d997d7ec88c185e14216767ff2c5",
+    "url": "/ReactMemoryGame/static/media/back.dd07d997.jpg"
+  }
+]);
