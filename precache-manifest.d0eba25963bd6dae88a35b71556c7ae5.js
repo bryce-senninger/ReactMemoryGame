060b0dc8efed4d1bffe722116216f31a8661f75b
Bryce <bryce.senninger@gmail.com>
2019-09-28 17:57:06 -0600

    Updates

---
+++ precache-manifest.d0eba25963bd6dae88a35b71556c7ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5701583f413c25ab55253ed6a18765",
+    "url": "/ReactMemoryGame/index.html"
+  },
+  {
+    "revision": "42c4222bd8589f9ca584",
+    "url": "/ReactMemoryGame/static/css/main.f0790404.chunk.css"
+  },
+  {
+    "revision": "e08cdf254e6015fe197e",
+    "url": "/ReactMemoryGame/static/js/2.90b068db.chunk.js"
+  },
+  {
+    "revision": "42c4222bd8589f9ca584",
+    "url": "/ReactMemoryGame/static/js/main.61d0d5fc.chunk.js"
+  },
+  {
+    "revision": "5f60813e77c63619c860",
+    "url": "/ReactMemoryGame/static/js/runtime-main.1049ada0.js"
+  },
+  {
+    "revision": "dd07d997d7ec88c185e14216767ff2c5",
+    "url": "/ReactMemoryGame/static/media/back.dd07d997.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bryce-senninger/ReactMemoryGame/precache-manifest.8056643a5f16cb683d81c4aa7444144f.js"
+  "/ReactMemoryGame/precache-manifest.d0eba25963bd6dae88a35b71556c7ae5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bryce-senninger/ReactMemoryGame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMemoryGame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
